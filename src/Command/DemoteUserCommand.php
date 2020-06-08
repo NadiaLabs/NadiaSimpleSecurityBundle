8eec349d173a81ad25ce7610d7989d6c10e35aa1
Leo <leo.on.the.earth@gmail.com>
2020-06-08 18:05:32 +0800

    Remove unnecessary imports

--- src/Command/DemoteUserCommand.php
+++ src/Command/DemoteUserCommand.php
@@ -11,7 +11,6 @@
 
 namespace Nadia\Bundle\NadiaSimpleSecurityBundle\Command;
 
-use Symfony\Component\Console\Helper\FormatterHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
